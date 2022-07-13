583f225edd6b2fa2cf139223bafb4f1daa123939
21021888 <21021888@LAPTOP-96PN58IG>
2022-07-13 12:15:04 +0800

    Commit by Jia Cheng

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -5,7 +5,7 @@ public class ResourceCentre {
 	public static void main(String[] args) {
 		//student 1 - Koh Wee Ren
 		//student 2 - Heather
-		// student 3 - Jia Cheng
+		//student 3 - Jia Cheng
 
 
 	
