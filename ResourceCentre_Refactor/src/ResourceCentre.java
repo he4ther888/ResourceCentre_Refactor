7b5131564a45343fe52b170290e208ae56a25bbf
21021888 <21021888@LAPTOP-96PN58IG>
2022-07-27 10:58:46 +0800

    Wee Ren, updated doReturnCamcorder()

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -273,6 +273,7 @@ public class ResourceCentre {
 		
 	}
 	//================================= Option 4 Return (CURD- Update)=================================
+	//By Wee Ren
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
 		boolean isReturned = false;
 
